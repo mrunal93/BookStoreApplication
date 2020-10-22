ebb58dda550c062200319fb8998d970a06329694
mrunal93 <mrn26893@gmail.com>
2020-10-22 09:23:40 +0530

    [Mrunal]Add:Cumtomer repository class with update and add value of customer

--- BookStoreApplication/RepositoryLayerBookStore/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RepositoryLayerBookStore
-{
-    public class Class1
-    {
-    }
-}
---
+++ BookStoreApplication/RepositoryLayerBookStore/CustomerRepositoryClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using ModelLayerBookStore;
+using System;
+using System.Data.SqlClient;
+
+namespace RepositoryLayerBookStore
+{
+    public class CustomerRepositoryClass
+    {
+        private readonly IConfiguration configuration;
+        private readonly string connectionString;
+        private readonly SqlConnection sqlConnection;
+
+        public CustomerRepositoryClass(IConfiguration configuration)
+        {
+
+            this.configuration = configuration;
+            this.connectionString = this.configuration.GetConnectionString("UserDbConnection");
+            this.sqlConnection = new SqlConnection(this.connectionString);
+        }
+
+        public CustomerRegistrationModelClass AddCustomerDetails (CustomerRegistrationModelClass customer)
+        {
+            SqlCommand sqlCommand = new SqlCommand("sp_AddCustomerDetails",sqlConnection);
+            return
+        }
+    }
+}
